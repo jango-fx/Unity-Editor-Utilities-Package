5792b3664fce0ea5250d81a2d9d01bbc03e740ec
jango-fx <felix.groll@gmail.com>
2021-05-04 16:12:00 +0200

    updated folder structure, asmdefs and fixed references

---
+++ ReadOnlyAttribute/Runtime/ReadOnlyAttribute.cs
@@ -0,0 +1,10 @@
+// ---------------------------------------------------------------------------- 
+// based on https://www.patrykgalach.com/2020/01/20/readonly-attribute-in-unity-editor/
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+namespace ArtCom.EditorUtilities
+{    
+    public class ReadOnlyAttribute : PropertyAttribute { }
+}   
